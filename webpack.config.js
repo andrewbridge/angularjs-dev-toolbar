1dc5875c92cf7b8d48e3a91472959de04594af63
Andrew Bridge <a.bridge@coinvestor.co.uk>
2018-04-22 20:00:08 +0100

    Turns Vue's production mode on if built with build:prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = env => {
-	console.log('Production: ', env.production);
+	console.log('Production: ', !!(env && env.production));
 	return {
 		entry: ['./src/index.js', './src/styles.scss'],
 		output: {
 			filename: 'bundle.js',
 			path: path.resolve(__dirname, 'dist')
 		},
-		mode: env.production ? 'production' : 'development',
+		mode: env && env.production ? 'production' : 'development',
 		resolve: {
 			alias: {
-				'vue$': 'vue/dist/vue.esm.browser.js'
+				'vue$': env && env.production ? 'vue/dist/vue.min.js' : 'vue/dist/vue.esm.browser.js'
 			}
 		},
 		module: {
@@ -26,6 +26,13 @@ module.exports = env => {
 					loader: "sass-loader" // compiles Sass to CSS
 				}]
 			}]
-		}
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: env && env.production ? '"production"' : '"development"'
+				}
+			})
+		]
 	};
 };
